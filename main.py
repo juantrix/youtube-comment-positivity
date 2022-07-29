583ab0bd25af78e3dfe4e5e9e0aa0022f4412945
Juan Ostrit <50851288+juantrix@users.noreply.github.com>
2022-07-29 12:49:23 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from pysentimiento import create_analyzer
+from os import system
+import json
+
+sentiment_analyzer = create_analyzer(task="sentiment", lang="es")
+
+# emotion_analyzer = create_analyzer(task="emotion", lang="es")
+
+# hate_speech_analyzer = create_analyzer(task="hate_speech", lang="es")
+url = input('Ingrese url del video: ')
+system(f'youtube-comment-downloader --url {url} --output comments.txt')
+
+data = open('comments.txt','r')
+sentimental_list = []
+
+for line in data:
+    text = json.loads(line)['text']
+    sentimental_list.append(sentiment_analyzer.predict(text).output)
+# for i in data:
+#     print(sentiment_analyzer.predict(i))
+print(f'''
+        Positivo: {round(sentimental_list.count('POS')/len(sentimental_list)*100, 2)}
+        Neutral: {round(sentimental_list.count('NEU')/len(sentimental_list)*100, 2)}
+        Negativo: {round(sentimental_list.count('NEG')/len(sentimental_list)*100, 2)}
+    ''')
