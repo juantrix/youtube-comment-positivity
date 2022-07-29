b691fd0c2c23c012e2e75553d94c63e1cf4749b3
Juan Ostrit <50851288+juantrix@users.noreply.github.com>
2022-07-29 13:33:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ leng = input('Select language(en/es): ')
 
 sentiment_analyzer = create_analyzer(task="sentiment", lang=leng)
 
-# emotion_analyzer = create_analyzer(task="emotion", lang="es")
-
-# hate_speech_analyzer = create_analyzer(task="hate_speech", lang="es")
 url = input('Video URL: ')
 system(f'youtube-comment-downloader --url {url} --output comments.txt')
 
@@ -18,8 +15,7 @@ sentimental_list = []
 for line in data:
     text = json.loads(line)['text']
     sentimental_list.append(sentiment_analyzer.predict(text).output)
-# for i in data:
-#     print(sentiment_analyzer.predict(i))
+
 print(f'''
         Positivo: {round(sentimental_list.count('POS')/len(sentimental_list)*100, 2)}
         Neutral: {round(sentimental_list.count('NEU')/len(sentimental_list)*100, 2)}
